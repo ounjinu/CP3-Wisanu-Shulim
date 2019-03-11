03553efa49f02834ff6fbafc0591db4efd0aed33
ounjinu <ounjinu@gmail.com>
2019-03-11 21:58:34 +0700

    Add Score

---
+++ Exercise4_Wisanu_S.py
@@ -0,0 +1,10 @@
+Eng = 95
+Biz = 90
+ComSys = 86
+ComPro = 89
+
+print("--- Your Score --- ")
+print("Foundation English :", Eng)
+print("General Business :", Biz)
+print("Introduction to Computer Systems :", ComSys)
+print("Computer Programming :", ComPro)
